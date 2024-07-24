cce2dd4037b2c77613dbe33f536c0193fd6b550a
paalamugan <paalamugan44@gmail.com>
2024-07-24 15:28:32 +0530

    ci(release): create a ci cd pipeline

---
+++ scripts/slack.ts
@@ -0,0 +1,43 @@
+import { readFile } from 'fs/promises';
+
+import { config } from 'dotenv';
+import fetch from 'node-fetch';
+
+config();
+
+async function getLatestVersion() {
+  const [content] = JSON.parse(await readFile('.changelog/manifest.json', 'utf8'));
+  return content.version;
+}
+
+export async function main() {
+  const url = process.env.SLACK_WEBHOOK_URL;
+
+  if (!url) {
+    throw new Error('Missing Slack Webhook URL');
+  }
+
+  const version = await getLatestVersion();
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      project: 'paalan-react-ui',
+      version,
+      url: `https://github.com/paalamugan/paalan-react-shadcn-ui/blob/main/.changelog/v${version}.mdx`,
+      discord: 'https://discordapp.com/channels/1234567890',
+      twitter: 'https://twitter.com/paalamugan',
+    }),
+  });
+
+  if (response.ok) {
+    console.log('✅ Successfully sent message to Slack');
+  } else {
+    console.error('❌ Failed to send message to Slack');
+  }
+}
+
+main();
